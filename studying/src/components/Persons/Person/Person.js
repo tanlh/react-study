02be64f184f4bb0968ad18de37e6eca15d997c8a
tan.lehuu <tan.lehuu@lazada.com>
2021-04-05 16:54:17 +0700

    fix css after restructure module

--- studying/src/components/Persons/Person/Person.js
+++ studying/src/components/Persons/Person/Person.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Person.css";
+import style from "./Person.css";
 
 const Person = (props) => {
   return (
-    <div className="Person">
+    <div className={style.Person}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
