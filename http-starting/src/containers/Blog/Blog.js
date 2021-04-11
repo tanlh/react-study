617ba45458f20d03b90980f5df505a2c1b5901fd
tan.lehuu <tan.lehuu@lazada.com>
2021-04-12 01:22:39 +0700

    axios interceptors and default config

--- http-starting/src/containers/Blog/Blog.js
+++ http-starting/src/containers/Blog/Blog.js
@@ -10,19 +10,23 @@ class Blog extends Component {
   state = {
     posts: [],
     selectedPostId: null,
+    error: false,
   };
 
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map((post) => {
-        return {
-          ...post,
-          author: "Tan, Le",
-        };
-      });
-      this.setState({ posts: updatedPosts });
-    });
+    axios
+      .get("/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map((post) => {
+          return {
+            ...post,
+            author: "Tan, Le",
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch((error) => this.setState({ error: true }));
   }
 
   postSelectedHandler = (id) => {
@@ -30,18 +34,21 @@ class Blog extends Component {
   };
 
   render() {
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      ));
+    }
+
     return (
       <div>
-        <section className="Posts">
-          {this.state.posts.map((post) => (
-            <Post
-              key={post.id}
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
-          ))}
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost id={this.state.selectedPostId} />
         </section>
