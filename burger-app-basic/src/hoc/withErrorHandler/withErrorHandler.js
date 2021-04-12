4b07a4d8282ca68d8d0ca949f6242bdf4587cf7e
tan.lehuu <tan.lehuu@lazada.com>
2021-04-12 19:07:15 +0700

    remove interceptors before component unmount

--- burger-app-basic/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-app-basic/src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,21 +9,24 @@ const withErrorHandler = (WrappedComponent) => {
     state = { error: null };
 
     componentWillMount() {
-      axios.interceptors.request.use(
+      this.reqInterceptors = axios.interceptors.request.use(
         (request) => {
           this.setState({ error: null });
           return request;
         },
         (error) => this.setState({ error: error })
       );
-      axios.interceptors.response.use(
-        (response) => {
-          return response;
-        },
+      this.resInterceptors = axios.interceptors.response.use(
+        (response) => response,
         (error) => {
           this.setState({ error: error });
         }
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptors);
+      axios.interceptors.response.eject(this.resInterceptors);
     }
 
     errorConfirmedHandler = () => {
