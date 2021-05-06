77057378fbdd575b7f460e632760618c90c7ae5c
tan.lehuu <tan.lehuu@lazada.com>
2021-05-06 22:32:55 +0700

    Assigment 4: conditional content

--- react-basic/src/components/NewExpense/NewExpense.js
+++ react-basic/src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [addingExpense, setAddingExpense] = useState(false);
+
   const saveExpenseHandler = (expenseData) => {
     const newExpense = {
       ...expenseData,
@@ -10,11 +13,27 @@ const NewExpense = (props) => {
     console.log('[NewExpense]', newExpense);
 
     props.onAddExpense(newExpense);
+    setAddingExpense(false);
+  };
+
+  const startAddingHandler = (_event) => {
+    setAddingExpense(true);
+  };
+
+  const cancelAddingHandler = () => {
+    setAddingExpense(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
+      {addingExpense ? (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseHandler}
+          onCancel={cancelAddingHandler}
+        />
+      ) : (
+        <button onClick={startAddingHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
