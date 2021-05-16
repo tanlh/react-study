b9f4f39c72d4ea81baacaace7dcc40d478fcdf89
tan.lehuu <tan.lehuu@lazada.com>
2021-05-16 23:40:53 +0700

    Section 18: setup redux study project

---
+++ redux-study/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>ahihi</h1>
+  );
+}
+
+export default App;
---
+++ redux-study/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+import axiosSetupGlobalConfig from 'utils/axiosConfig';
+
+ReactDOM.render(<App />, document.getElementById('root'));
