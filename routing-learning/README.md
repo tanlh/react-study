497b653352f9ddeb45f035fd880adc37b5b4e5b4
tan.lehuu <tan.lehuu@lazada.com>
2021-05-19 00:16:05 +0700

    Section 20: finished routing learning project

---
+++ routing-learning/README.md
@@ -0,0 +1,1 @@
+# Learning react routing project
---
+++ routing-learning/src/App.js
@@ -0,0 +1,32 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import Welcome from 'pages/Welcome';
+import Products from 'pages/Products';
+import ProductDetail from 'pages/ProductDetail';
+import MainHeader from 'components/MainHeader';
+
+function App() {
+  return (
+    <>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
+
+export default App;
